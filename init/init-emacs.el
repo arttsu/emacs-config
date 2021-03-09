4b2c0b8d14032e9decd4ccf87b849e300d6c8a5f
Artem Tseranu <artem.tseranu@xing.com>
2021-03-09 09:37:36 +0100

    Bind keys to enlarge/shrink window

--- init/init-emacs.el
+++ init/init-emacs.el
@@ -48,6 +48,11 @@
 
 (global-set-key (kbd "C-c q") 'delete-window)
 
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
 (provide 'init-emacs)
 
 ;;; init-emacs.el ends here
