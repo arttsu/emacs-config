5c0f4aa24edc36769c8c3e4ae8d701cde39f4214
Artem Tseranu <artem.tseranu@xing.com>
2021-07-02 09:58:20 +0200

    tmp

--- init/init-master-hydras.el
+++ init/init-master-hydras.el
@@ -17,9 +17,21 @@
   ("o" (find-file "~/zk/xing-operational-data-tracking.org") "ODT")
   ("t" (find-file "~/zk/xing-topic-classifier.org") "Topic Classifier"))
 
+(defhydra my-hydra-open-bookmarks (:color blue)
+  "Bookmarks"
+  ("b" (find-file "~/journal/bookmarks.org") "Bookmarks")
+  ("w" (find-file "~/journal/work-bookmarks.org") "Work bookmarks"))
+
+(defhydra my-hydra-open-requests (:color blue)
+  "Requests"
+  ("c" (find-file "~/projects/xing/content/content-requests.http") "Content requests")
+  ("n" (find-file "~/projects/xing/news-data/news-recommender-requests.http") "News recommender requests"))
+
 (defhydra my-hydra-open (:color blue)
   "Open"
+  ("b" my-hydra-open-bookmarks/body "Bookmarks")
   ("p" (find-file "~/nb/projects.org") "Projects")
+  ("r" my-hydra-open-requests/body "Requests")
   ("w" (find-file "~/nb/work-projects.org") "Work projects")
   ("z" my-hydra-open-zk/body "Zettel"))
 
